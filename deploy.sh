81a2c75fb7864ed3ede994e551adb1f859a079e3
Administrator <admin@example.com>
2022-11-18 03:36:16 +0000

    更新 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ if [[ -n $(docker ps -q -f "name=^hugo$") ]];then
 elif [[ -n $(docker ps -aq -f "name=^hugo$") ]];then
         echo "hugo 已经停止"
         docker rm -f hugo
-        docker run --name hugo -p 80:80 -p 443:443 -d -v ${HOME}/blog//Caddyfile:/etc/caddy/Caddyfile -v ${HOME}/blog/site:/var/www  -v ${HOME}/blog/config:/config -v ${HOME}/blog/data:/data -e TZ=Asia/Shanghai caddy
+        docker run --name hugo -p 80:80 -p 443:443 -d -v ${HOME}/blog/Caddyfile:/etc/caddy/Caddyfile -v ${HOME}/blog/site:/var/www  -v ${HOME}/blog/config:/config -v ${HOME}/blog/data:/data -e TZ=Asia/Shanghai caddy
 else
         echo "没有Hugo服务"
         mkdir $HOME/blog/
-        docker run --name hugo -p 80:80 -p 443:443 -d -v ${HOME}/blog//Caddyfile:/etc/caddy/Caddyfile -v ${HOME}/blog/site:/var/www  -v ${HOME}/blog/config:/config -v ${HOME}/blog/data:/data -e TZ=Asia/Shanghai caddy
+        docker run --name hugo -p 80:80 -p 443:443 -d -v ${HOME}/blog/Caddyfile:/etc/caddy/Caddyfile -v ${HOME}/blog/site:/var/www  -v ${HOME}/blog/config:/config -v ${HOME}/blog/data:/data -e TZ=Asia/Shanghai caddy
 fi
