82ff481d9e4d94cf27503d59f4477f925b13a25f
Administrator <admin@example.com>
2022-11-18 04:21:22 +0000

    更新 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,13 @@ if [[ -n $(docker ps -q -f "name=^hugo$") ]];then
 elif [[ -n $(docker ps -aq -f "name=^hugo$") ]];then
         echo "hugo 已经停止"
         docker rm -f hugo
+        cp -r site $HOME/blog
+        cp Caddyfile $HOME/blog/Caddyfile
         docker run --name hugo -p 80:80 -p 443:443 -d -v ${HOME}/blog/Caddyfile:/etc/caddy/Caddyfile -v ${HOME}/blog/site:/var/www  -v ${HOME}/blog/config:/config -v ${HOME}/blog/data:/data -e TZ=Asia/Shanghai caddy
 else
         echo "没有Hugo服务"
         mkdir $HOME/blog/
+        cp -r site $HOME/blog
+        cp Caddyfile $HOME/blog/Caddyfile
         docker run --name hugo -p 80:80 -p 443:443 -d -v ${HOME}/blog/Caddyfile:/etc/caddy/Caddyfile -v ${HOME}/blog/site:/var/www  -v ${HOME}/blog/config:/config -v ${HOME}/blog/data:/data -e TZ=Asia/Shanghai caddy
 fi
